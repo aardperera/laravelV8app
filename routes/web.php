bec50f783d846a1316cd48a65be89430d8fcccee
Ranjana Perera <76731513+aardperera@users.noreply.github.com>
2024-01-31 15:24:09 +0530

    add store route

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('/todo', [TodoController::class, 'index'])->name('todo');
+
+Route::prefix('/todo')->group(function (){
+    Route::get('/', [TodoController::class, 'index'])->name('todo');
+    Route::post('/store', [TodoController::class, 'store'])->name('todo.store');
+});
