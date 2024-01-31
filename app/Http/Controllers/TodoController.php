3b81a886f1dacac43fa7935ea0c3b7b98cd02976
Ranjana Perera <76731513+aardperera@users.noreply.github.com>
2024-01-31 15:27:55 +0530

    create store function to add tasks to db

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -1,12 +1,24 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Todo;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
+    protected $task;
+    public function __construct(){
+        $this->task= new Todo();
+
+    }
+
     public function index(){
         return view('pages.todo.index');
+    }
+
+    public function store(Request $request){
+        $this->task->create($request->all());
+
+        return redirect()->back();
     }
 }
