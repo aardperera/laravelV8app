9faca3de79f2a9f19a222f0bbd6bc7b626379a6e
Ranjana Perera <76731513+aardperera@users.noreply.github.com>
2024-01-31 12:10:10 +0530

    include css js navbar and content

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Todo</title>
+
+    @include('libraries.styles')
 </head>
 <body>
+    @include('components.nav')
+    @yield('content')
 
+    @include('libraries.scripts')
 </body>
 </html>
