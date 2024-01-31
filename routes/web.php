46262e7d37b4b617385652fef070345a4ff73972
Ranjana Perera <76731513+aardperera@users.noreply.github.com>
2024-01-31 10:23:41 +0530

    update todocontroller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,3 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('/todo', [TodoController::class, 'index'])->name('todo');
