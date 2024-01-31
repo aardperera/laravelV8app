c20fb24351473f67ee57b13cafd31ae71d191328
Ranjana Perera <76731513+aardperera@users.noreply.github.com>
2024-01-31 12:09:07 +0530

    create home page

--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -1,1 +1,23 @@
+@extends('layouts.app')
 
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h1 class="page-title">Home Page</h1>
+            </div>
+        </div>
+    </div>
+@endsection
+
+
+@push('css')
+<style>
+    .page-title{
+        font-size: 5rem;
+        padding-top: 15vh;
+        color: #6161bb;
+    }
+</style>
+
+@endpush
